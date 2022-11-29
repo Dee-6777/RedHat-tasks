bc9d70e771a0f83eb49bc8481f2353814ebead00
Deeptimayee Maharana <dmaharan@redhat.com>
2022-11-29 17:33:28 +0530

    Add files via upload

--- Task 2/02_code.sh
+++ Task 2/02_code.sh
@@ -1,20 +1,2 @@
-# with using associative arrays
-# Time Complexity: O(n)
-# Space Complexity: O(n)
-#!/bin/bash
 arr=( `cat "IP addresses.txt" `)
-declare -A freq
-max_times=0
-for i in "${arr[@]}"; 
-do
-  ((freq[$i]++))
-  h=${freq[$i]}
-  if [[ $h > $max_times ]]; 
-  then
-    max=$i
-    max_times=$h
-  fi
-done
-
-printf '%s\n' "$max"
-printf '%d\n' "$max_times"
+printf '%s\n' "${arr[@]}" | sort -n | uniq -c | sort -n | tail -1
