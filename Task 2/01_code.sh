46e2932a0d1a0bc63ab2b171138fe926664c63a9
Deeptimayee Maharana <dmaharan@redhat.com>
2022-11-29 15:11:12 +0530

    Update 01_code.sh

--- Task 2/01_code.sh
+++ Task 2/01_code.sh
@@ -1,12 +1,15 @@
 # with using associative arrays
+
 #!/bin/bash
 arr=( `cat "IP addresses.txt" `)
 declare -A hash
 max_times=0
-for i in "${arr[@]}"; do
+for i in "${arr[@]}"; 
+do
   ((hash[$i]++))
   h=${hash[$i]}
-  if [[ $h > $max_times ]]; then
+  if [[ $h > $max_times ]]; 
+  then
     max=$i
     max_times=$h
   fi
