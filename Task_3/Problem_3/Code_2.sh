e3812c7f67591b12b6cbe9564bc7881321cbb257
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 04:14:15 +0530

    Update Code_2.sh

--- Task_3/Problem_3/Code_2.sh
+++ Task_3/Problem_3/Code_2.sh
@@ -1,4 +1,4 @@
-# Time Complexity : O(n) ; where n is the size of the input
+# Time Complexity : O(n)
 
 # Sum of the numbers (float, int, -ve)
 
@@ -13,9 +13,18 @@ done
 
 echo "sum = ${total}" 
 
+#Expln:
+# Finds and discards the strings which are found to be unsuitable for addition
+# Time Complexity will be O(n) where n represents the size of the input or the length of the array.
+# Addition occurs for the remaining suitable strings.
+
+
+
+
+# Few additional terms :
 # additional expression added for covering corner casses such as alpha-numeric characters, special characters etc..
-# Here, ^ means it matches the empty string at the beginning of a line
-# [0-9] : represents the range 
+# Here, ^ : matches the empty string at the beginning of a line
+# - : represents the range 
 # + : The preceding item will be matched one or more times [1,]
 # ? : The preceding item is optional and will be matched, at most, once [0,1]
 # $ : Matches the empty string at the end of a line
