997226f77f2816f527dfa7e2f9ce5f3a9ca3cc38
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 03:39:24 +0530

    Update Code1.sh

--- Task_3/Problem_1/Code1.sh
+++ Task_3/Problem_1/Code1.sh
@@ -6,6 +6,7 @@
 # %S   second (00..60)
 
 # Time Complexity : O(1)
+# Space Complexity : O(1)
 
 echo -e "Current Day : $(date +%A)\nCurrent Month : $(date +%B)\nCurrent Year : $(date +%Y)\nHours : $(date +%H)\nMinutes : $(date +%M)\nSeconds : $(date +%S)" 
 
