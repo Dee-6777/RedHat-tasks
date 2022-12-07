c2b059c43b9360d709ac81695ae6c8e0ee938561
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-07 21:12:47 +0530

    Update Code_2.sh

--- Task_3/Problem_3/Code_2.sh
+++ Task_3/Problem_3/Code_2.sh
@@ -2,11 +2,17 @@
 
 # Sum of the numbers (float, int, -ve)
 
-total=0
+# Time Complexity : O(n)
 
+# Sum of the numbers (float, int, -ve)
+
+total=0
+re='^[+-]?[0-9]+([.][0-9]+)?$'
 for i in $(cat file2.txt); do
-  total=$(echo $total + $i | bc);  # bc : basic calculator 
+  if [[ $i =~ $re ]];
+  then
+    total=$(echo $total + $i | bc);  # bc : basic calculator 
+  fi
 done
 
 echo "sum = ${total}" 
-
