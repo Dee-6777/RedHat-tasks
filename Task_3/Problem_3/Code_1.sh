d142577eb002ae1d86688a8fa4233a00cb0d5eac
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 03:41:20 +0530

    Update Code_1.sh

--- Task_3/Problem_3/Code_1.sh
+++ Task_3/Problem_3/Code_1.sh
@@ -1,10 +1,5 @@
 # Time Complexity : O(n)
 
-# Generates Random numbers
-for((i=0;i<10000;i++));
-do
-    echo $RANDOM >> file1.txt     #redirects the output of the command or data to end of file file1.txt
-done
 
 # Finds the sum of the integers
 sum=0
