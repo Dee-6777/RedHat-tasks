ea7db92e62bac59e9ad2fb0c512e9d752090c977
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 02:34:56 +0530

    Update Code_2.sh

--- Task_3/Problem_3/Code_2.sh
+++ Task_3/Problem_3/Code_2.sh
@@ -2,11 +2,8 @@
 
 # Sum of the numbers (float, int, -ve)
 
-# Time Complexity : O(n)
-
-# Sum of the numbers (float, int, -ve)
-
 total=0
+
 re='^[+-]?[0-9]+([.][0-9]+)?$'
 for i in $(cat file2.txt); do
   if [[ $i =~ $re ]];
