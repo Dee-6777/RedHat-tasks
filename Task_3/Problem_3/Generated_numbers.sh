1e24777ae87ad1cd9b8ae8a315a2c15189c04713
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 03:40:46 +0530

    Create Generated_numbers.sh

---
+++ Task_3/Problem_3/Generated_numbers.sh
@@ -0,0 +1,7 @@
+# Time Complexity : O(n)
+
+# Generates Random numbers
+for((i=0;i<10000;i++));
+do
+    echo $RANDOM >> file1.txt     #redirects the output of the command or data to end of file file1.txt
+done
