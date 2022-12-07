2c493764ad0fdd4861183072ffb0e31f925b932f
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 03:37:05 +0530

    Update Code_2.sh

--- Task_3/Problem_3/Code_2.sh
+++ Task_3/Problem_3/Code_2.sh
@@ -3,13 +3,19 @@
 # Sum of the numbers (float, int, -ve)
 
 total=0
-
-re='^-?[0-9]+[.]?[0-9]+$' 
-for i in $(cat file2.txt); do
-  if [[ $i =~ $re ]];
+rexprr='^-?[0-9]+[.]?[0-9]+$' #?
+for i in $(cat file2.txt); do 
+  if [[ $i =~ $rexprr ]];   # this is a tilde symbol which is used for the expressions ?
   then
     total=$(echo $total + $i | bc);  # bc : basic calculator 
   fi
 done
 
 echo "sum = ${total}" 
+
+# additional expression added for covering corner casses such as alpha-numeric characters, special characters etc..
+# Here, ^ means it matches the empty string at the beginning of a line
+# - : represents the range 
+# + : The preceding item will be matched one or more times [1,]
+# ? : The preceding item is optional and will be matched, at most, once [0,1]
+# $ : Matches the empty string at the end of a line
