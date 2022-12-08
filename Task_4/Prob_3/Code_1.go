42b5a23054ec2d46234668b41d337710bf0afee5
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-08 22:01:19 +0530

    Add files via upload

---
+++ Task_4/Prob_3/Code_1.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	students := [5]string{"Aditya", "Deepti", "Sai", "Arpita", "Harsh"}
+	for i, k := range students {
+		fmt.Printf("The name of the students is %s and the position of the student in the array is %v\n", k, i)
+	}
+}
