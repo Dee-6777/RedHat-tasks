0d807cd3581906c75a82dbaee95517155f786222
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-13 00:01:57 +0530

    Update Code_1.go

--- Go Task-2/Prob_3/Code_1.go
+++ Go Task-2/Prob_3/Code_1.go
@@ -6,7 +6,7 @@ package main
 import (
 	"fmt"       //fmt stands for the Format package. This package allows to format basic strings, values, or anything and print
 	"math/rand" //  provides inbuilt support for generating random numbers of the specified type
-	"time"      //provides current time and in rand.Seed it is initialised with the current time expressed in microseconds
+	"time"       //provides current time and it initialises rand.Seed with the current time which is expressed in microseconds
 )
 
 func main() {
