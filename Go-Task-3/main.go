353ee8a4f09d61a99002d1736b58c96d5b129df3
Deeptimayee Maharana <dmaharan@redhat.com>
2022-12-15 00:38:53 +0530

    Task-3 added

---
+++ Go-Task-3/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json" //to encode it to json
+	"fmt"
+	"log"
+
+	//not
+	"net/http" //not
+	//"strconv"
+	//"math/rand"
+	"github.com/gorilla/mux"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Duration  string `json:"time"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Duration: "10 min", Completed: false},
+	{ID: "2", Item: "Brush Teeth", Duration: "10 min", Completed: false},
+	{ID: "3", Item: "Go For a Walk", Duration: "15 min", Completed: false},
+	{ID: "4", Item: "Breakfast", Duration: "20 min", Completed: false},
+	{ID: "5", Item: "Study", Duration: "1 hour", Completed: false},
+	{ID: "6", Item: "Attend Meeting", Duration: "30 min", Completed: false},
+}
+
+//1
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
+
+//2
+func deleteTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range todos {
+		if item.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(todos)
+}
+
+//3
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range todos {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+}
+
+//4
+func createTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var todo todo
+	_ = json.NewDecoder(r.Body).Decode(&todo)
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(todo)
+}
+
+//5
+func updateTodo(w http.ResponseWriter, r *http.Request) {
+	//set json content type
+	w.Header().Set("Content-Type", "application/json")
+	//params
+	params := mux.Vars(r)
+	//loop over the todos, range
+	//delete the todo with the id that you've sent
+	//add a new todo - the movie that we send in the body of postman
+	for index, item := range todos {
+		if item.ID == params["id"] {
+			todos = append(todos[:index], todos[index+1:]...)
+			var todo todo
+			_ = json.NewDecoder(r.Body).Decode(&todo)
+			todo.ID = params["id"]
+			todos = append(todos, todo)
+			json.NewEncoder(w).Encode(todo)
+		}
+	}
+}
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/todos", getTodos).Methods("GET")
+	r.HandleFunc("/todos/{id}", getTodo).Methods("GET")
+	r.HandleFunc("/todos", createTodo).Methods("POST")
+	r.HandleFunc("/todos/{id}", updateTodo).Methods("PUT")
+	r.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
